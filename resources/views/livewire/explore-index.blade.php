9b989157e26579c87f07d39d830dabd14c1c7ef1
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2023-03-14 15:52:29 +0700

    update: card article

--- resources/views/livewire/explore-index.blade.php
+++ resources/views/livewire/explore-index.blade.php
@@ -18,15 +18,18 @@
         <div class="row">
             @for ($i = 1; $i <= 12; $i++)
                 <div class="col-md-3 mb-4 d-flex align-self-stretch">
-                    <a href="{{ route('articles.show', 'slug') }}"
-                        class="card text-decoration-none overflow-hidden border-0">
+                    <a href="{{ route('articles.show', 'slug') }}" class="card text-decoration-none border-0">
+                        <img src="{{ asset('img/thumbnail.jpg') }}" class="img-fluid rounded mb-3" alt="">
                         <div class="d-flex flex-column relative h-100">
-                            <img src="{{ asset('img/thumbnail.jpg') }}" class="img-fluid rounded mb-3" alt="">
                             <p class="text-muted mb-2">{{ now()->format('d M Y') }}</p>
                             <h6 class="fw-semibold text-dark lh-base">Lorem ipsum dolor sit amet consectetur,
                                 adipisicing
                                 elit.
                             </h6>
+
+                            <div class="mt-auto">
+                                <p class="text-muted">Programming</p>
+                            </div>
                         </div>
                     </a>
                 </div>
